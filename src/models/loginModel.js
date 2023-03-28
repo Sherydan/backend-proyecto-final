c3003683682468ebee8f54336d817e377e6b70f8
Sherydan <tobar.luis.c@gmail.com>
2023-03-28 19:14:17 -0300

    fix users and login query

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -2,7 +2,7 @@ const {pool} = require("../helpers/connectionDB")
 const verifyCredentials = async (email) => {
     try {
         const values = [email];
-        const consulta = "SELECT * FROM usuarios WHERE email = $1";
+        const consulta = "SELECT * FROM users WHERE email = $1";
         const result = await pool.query(consulta, values);
 
         return result.rows;
