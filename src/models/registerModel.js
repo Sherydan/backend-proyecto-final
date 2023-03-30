87bdd4d70a9f272d01783cfbab47ffb8cb3c7a98
Pablo Goeppinger <pgoeppin@gmail.com>
2023-03-29 23:28:59 -0300

    feat:Adding the registerModel,js

---
+++ src/models/registerModel.js
@@ -0,0 +1,82 @@
+const { pool } = require("../helpers/connectionDB")
+const bcrypt = require("bcryptjs");
+
+
+const registerUserAndStore = async (user, store) => {
+    try {
+        let { email, password, role, first_name, last_name } = user;
+        let { name, rut, industry, address } = store;
+        const encriptedPassword = bcrypt.hashSync(password);
+        password = encriptedPassword;
+        const userValues = [
+            email,
+            encriptedPassword,
+            role,
+            first_name,
+            last_name,
+        ];
+        const storeValues = [
+            name,
+            rut,
+            industry,
+            address,
+        ];
+        const userQuery =
+            "INSERT INTO users (email, password, role, first_name, last_name) VALUES ($1, $2, $3, $4, $5)";
+        const storeQuery =
+            "INSERT INTO store (name, rut, industry, address) VALUES ($1, $2, $3, $4)";
+        const userResult = await pool.query(userQuery, userValues);
+        const storeResult = await pool.query(storeQuery, storeValues);
+        const userRowCount = userResult.rowCount;
+        const storeRowCount = storeResult.rowCount;
+    
+        if (!userRowCount || !storeRowCount) {
+            throw {
+                code: 404,
+                message: "Cant create user",
+            };
+        }
+    
+        return userResult.rows;
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+};
+
+// check if user already exists, returns true if exists, false if not
+const checkIfUserAlreadyExists = async ({ email }) => {
+    try {
+        const query = "SELECT * FROM users WHERE email = $1";
+        const values = [email];
+        const result = await pool.query(query, values);
+
+        if (result.rowCount >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const checkIfStoreAlreadyExists = async ({ rut }) => {
+    try {
+        const query = "SELECT * FROM store WHERE rut = $1";
+        const values = [rut];
+        const result = await pool.query(query, values);
+
+        if (result.rowCount >= 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { registerUserAndStore, checkIfUserAlreadyExists, checkIfStoreAlreadyExists };
