cb416628981c629af3e127cf82c1130fe7a82ca7
VictorGonzalezGomez <v.gonzalezgomez.vg@gmail.com>
2023-03-31 03:16:43 -0300

    add: test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 // app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 
 module.exports = app;
