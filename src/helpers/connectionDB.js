502fa20f8174cac5e52e59041d8e77d5e8f78a0f
Sherydan <tobar.luis.c@gmail.com>
2023-03-31 04:44:57 -0300

    added start script

--- src/helpers/connectionDB.js
+++ src/helpers/connectionDB.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 const config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     allowExitOnIdle: true,
 };
 
