9f2b095ed52cbab739211caf20bb70cc66b9e6dc
Sherydan <tobar.luis.c@gmail.com>
2023-03-31 04:49:53 -0300

    deleted validate user

--- src/helpers/validateNewuser.js
+++
@@ -1,9 +0,0 @@
-const checkUserFields = ({email, password, role,first_name, last_name}) => {
-    if (!email || !password || !role || !first_name || !last_name) {
-        return true
-    } else {
-        return false
-    }
-}
-
-module.exports = {checkUserFields}
