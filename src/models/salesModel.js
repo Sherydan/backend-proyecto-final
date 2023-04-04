c581f6075171fd8d38fbf753100b6572e66b367d
Sherydan <tobar.luis.c@gmail.com>
2023-04-03 21:10:16 -0400

    fix store id on model

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -4,14 +4,15 @@ const { pool } = require("../helpers/connectionDB");
 // needs to insert each sale into the database
 const addSales = async (sales) => {
     let result = null;
+    
     try {
         // for each sale in the array
         console.log("sales en model", sales);
         for (let sale of sales) {
             // destructure the sale
-            let { date, id, product_id, product_description, store_id, total_sold, unit_price, units_sold } = sale;
+            let { date, id, product_id, product_description, total_sold, unit_price, units_sold } = sale;
             // create the values array
-            const values = [product_id, product_description, store_id, unit_price, total_sold, units_sold, date];
+            const values = [product_id, product_description, id, unit_price, total_sold, units_sold, date];
             // create the query
             const consulta =
                 "INSERT INTO product_sales VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7)";
